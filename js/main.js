5fdc86d420cee606fecae2079dcf6d90b3fc2df7
stchambers89 <56280693+stchambers89@users.noreply.github.com>
2020-09-16 13:58:51 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,8 +18,11 @@ links.forEach(fill_weeks())
 
 function fill_weeks(item, index)
 {
+  times = 1;
+  document.getElementById("test").innerHTML = "this has gone through " + times;
   var listItem = document.createElement("a");
   listItem.setAttribute("href", links.url); 
   listItem.innerText = links.label;  
   li.appendChild(listItem);
+  times++;
 }
