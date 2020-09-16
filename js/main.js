54c8b202f21c2d7b464fb3012a7856283c500907
stchambers89 <56280693+stchambers89@users.noreply.github.com>
2020-09-16 09:57:13 -0700

    link the url to the text.
    Trying to figure out how to get the child element to not only pick up the text of the array, but also the URL.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const links =
 [
   {
     label: "Week 1 notes"
+    url: "week1/index.html"
     
   },
 
@@ -16,9 +17,11 @@ var ol = document.querySelector("ol");
 for (var i = 0; i < links.length; i++) 
 {
   var text = links[i].label;
+  var url = links[i].url
 
   var listItem = document.createElement("ol");
   listItem.textContent = text;
+  var listUrl = 
 
   ol.appendChild(listItem);
 }
