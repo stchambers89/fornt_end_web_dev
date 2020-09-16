4a24bc9a216772d1802293c9cddfa97dc16ff97e
stchambers89 <56280693+stchambers89@users.noreply.github.com>
2020-09-15 19:15:56 -0700

    Attempt 1: dynamic creation of ordered list on main page

--- js/main.js
+++ js/main.js
@@ -2,5 +2,20 @@ const links = [
   {
     label: "Week1 notes",
     url: "week1/index.html"
+  },
+  {
+    label: "Week 2 notes",
+    url: "week2/index.html"
   }
 ]
+
+var ol = document.querySelector("ol");
+
+for (var i = 0; i < links.length; i++) {
+  var link = links[i].label;
+
+  var listItem = document.createElement("ol");
+  listItem.textContent = link.url;
+
+  ol.appendChild(listItem);
+}
