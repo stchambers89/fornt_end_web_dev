abcee7fe83c1673016b1c9c0cc5ba7a5cc67d006
stchambers89 <56280693+stchambers89@users.noreply.github.com>
2020-09-17 15:16:54 -0700

    Update main.js
    Added weeks 3 - 14

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
 const links = 
 [
-  {
-    label: "Week 1 notes",
-    url: "week1/index.html"
-    
-  },
-
-  {
-    label: "Week 2 notes",
-    url: "week2/index.html"
-  }
+  {label: "Week 1 notes", url: "week1/index.html"},
+  {label: "Week 2 notes", url: "week2/index.html"},
+  {label: "Week 3 notes", url: "week3/index.html"},
+  {label: "Week 4 notes", url: "week4/index.html"},
+  {label: "Week 5 notes", url: "week5/index.html"},
+  {label: "Week 6 notes", url: "week6/index.html"},
+  {label: "Week 7 notes", url: "week7/index.html"},
+  {label: "Week 8 notes", url: "week8/index.html"},
+  {label: "Week 9 notes", url: "week9/index.html"},
+  {label: "Week 10 notes", url: "week10/index.html"},
+  {label: "Week 11 notes", url: "week11/index.html"},
+  {label: "Week 12 notes", url: "week12/index.html"},
+  {label: "Week 13 notes", url: "week13/index.html"},
+  {label: "Week 14 notes", url: "week14/index.html"},
 ]
+
 
 const myList = document.getElementById("weekList");
 
