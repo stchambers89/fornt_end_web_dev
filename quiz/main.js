daf75fd6b941ab784a7745fae240327365583675
stchambers89 <56280693+stchambers89@users.noreply.github.com>
2020-09-24 16:15:30 -0700

    Week 2 notes - reasearched string concat and backticks as well as == vs === quiz.main.js - added array, and question logic

--- quiz/main.js
+++ quiz/main.js
@@ -1,1 +1,27 @@
 alert('Welcome to Quiz Ninja!');
+
+const quiz = 
+[
+    ["What is Superman's real name?","Clark Kent"],
+    ["What is Wonder Woman's real name?","Diana Prince"],
+    ["What is Batman's real name?","Bruce Wayne"]
+];
+
+let score = 0;
+
+for( const [question, answer] of quiz)
+{
+    const response = prompt(question);
+    if(response === answer)
+    {
+        alert('Correct!');
+        score++;
+    }
+    else
+    {
+        //had to study using backticks and ${var}- pretty cool stuff. 
+        alert(`Wrong- the coorect answer is ${answer}`);
+    }
+}
+
+alert(`Game Over man, Game Over, you sored ${score} points:`);
--- week2/notes.js
+++ week2/notes.js
@@ -51,3 +51,75 @@ let arrayLength = cars.length(); // array length is now 6
 
 let firstcar = cars.shift(); //firstcar is equal to "ford"
 
+
+/*******************************************************************************
+* ---- == vs === -------
+* Simply put the more ='s there are the more strict the compaison. when === is
+* used both the value and type must be the same. When using == however, it
+* looks for loose equality. So it trys to convert the value and or type into
+* a common type. 
+* See Below:
+*******************************************************************************/
+
+const number1 = 5;
+const number2 = 5;
+
+if(number1 === number2) // equates to true same TYPE, same VALUE. 
+{
+    console.log("they are the same!"); //this will fire
+}
+
+const five = '5';
+
+if(number1 === five) // types not the same int != string
+{
+    console.log("they are the same!");
+}
+else
+{
+    console.log("these are different"); //this will fire
+}
+
+//loose compare
+if(number1 == five) // will try to convert either number1 to a string, or five to an int. 
+{
+    console.log("they are the same!");//this should fire. 
+}
+else
+{
+    console.log("these are different");
+}
+
+/*******************************************************************************
+* ---- String Concatenation options ----
+*******************************************************************************/
+// Using + and =
+//used to doing this, but it can be long to type out, and kind of hard to read
+let greeting = "hello " + "world";
+
+//using shorthand +=
+greeting += " how are you doing?"
+
+
+
+//join arrays:
+let holiday = ['Twas', 'the', 'night', 'before', 'Christmas'].join(' ');
+// 'Twas the night before Christmas'
+
+//the charater within the join() will be inserted bewteen each element of the array. 
+let holiday = ['Twas', 'the', 'night', 'before', 'Christmas'].join('1');
+// 'Twas1the1night1before1Christmas' becasue we said place the 1 in the middle. 
+
+//using backticks - lets say we have a person object that id's male/female. 
+//this is not an boject just a simple variable, bu tit will work
+
+let personGender = "male";
+
+if(personGender === "male")
+{
+    console.log(`The person is a ${personGender}`);
+}
+else
+{
+    console.log(`the person is female`);
+}
