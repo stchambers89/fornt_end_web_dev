42220e6ee077e7fc6dbb50c340f4fb16ef7be8e5
stchambers89 <56280693+stchambers89@users.noreply.github.com>
2020-09-30 16:37:33 -0700

    Create stdDeviation.js

---
+++ week3/stdDeviation.js
@@ -0,0 +1,39 @@
+function stdDeviation(...args)
+{
+    const setLength = args.length;
+    const mean = mean(args);
+    let deviation = getDeviation(args);
+
+
+    function mean(...values)
+    {
+        let total = 0;
+        for(const value of values)
+        {
+            total += value;
+        }
+        return total/setLength;
+    }
+
+    
+    //manipulates args index-mean^2
+    for(let index = 0; index < setLength; i++)
+    {
+        args[i] = Math.pow((args[i] - mean), 2);
+    }
+
+    //adds manipulated elements and averages
+    function getDeviation(...values)
+    {
+        for(const value of values)
+        {
+        total += value;
+        let deviation = total / setLength;
+        }
+        return deviation;
+    }
+
+    let finalTotal = Math.sqrt(deviation);
+
+
+}
