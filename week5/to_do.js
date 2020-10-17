5bb7704fdeee37fe527e7f671493316365ecf71d
stchambers89 <56280693+stchambers89@users.noreply.github.com>
2020-10-16 19:05:01 -0700

    adds toDo when button or enter, refocus & clear

--- week5/to_do.js
+++ week5/to_do.js
@@ -2,38 +2,51 @@ let list = [];
 
 //Get variable elements from HTML
 const theList = document.getElementById("theList");
-const input = document.getElementById("input");
+const input = document.querySelector("#input");
 
 //Get class names from HTML
 const CHECK = "fas fa-check-circle"
 const UNCHECK = "far fa-arrow-alt-circle-right"
 const CROSSOUT = 
 
-document.addEventListener('keyup', function (e) 
+
+input.addEventListener('keypress', function (e) 
 {
-    if (e.key === 'Enter') 
+    if (e.key === 'Enter') {
+        enter();
+      }
+  });
+
+
+
+function enter()
+{
+    if(input !== "")
     {
         const item = input.value;
         if(item)
         {
             addItem(item);
-            list.push(
+            /*list.push(
                     { 
                         name: item,
                         itemNumber: itemNumber
                     });
+        
+        itemNumber++;*/
         }
-        document.getElementById('input').value = '';
-        itemNumber++;
-
     }
-  });
+    document.getElementById('input').value = '';
+    document.getElementById("input").focus();
+}
+
 
 function addItem(item)
 {
     const toDoItem = `<li class="item">
                     <i class="far fa-arrow-alt-circle-right onclick="complete()"</i>
                     <p class="text"> ${item}</p>
+                    <i class="fas fa-times-circle"</i>
                     </li>`
 
 theList.insertAdjacentHTML("beforeend", toDoItem);
