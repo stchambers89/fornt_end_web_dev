b56d4c201a21b8f01dd702fb8f1766ffa6a38e96
stchambers89 <56280693+stchambers89@users.noreply.github.com>
2020-10-20 13:23:37 -0700

    restructure class to build and handle input

--- week5/to_do.js
+++ week5/to_do.js
@@ -3,29 +3,97 @@ let list = [];
 //Get variable elements from HTML
 const theList = document.getElementById("theList");
 const input = document.querySelector("#input");
+const icon = document.getElementById("icon");
 let idNumber = 0;
 
+icon.addEventListener('click', function(event)
+{
+    alert("i've been clicked");
+});
 
 /// Need event listener connected to the id number of the icon element
 
 
 //Define class
-class Todo {
-    constructor(item, id)
+let Task = {
+    selectElements: function()
     {
-        this.toDo = item;
-        this.id = id;
-        this.trash = false;
-        this.completed = false;
-        this.icon = "far fa-arrow-alt-circle-right";
-    }
+        this.taskInput = document.getElementById("input_task");
+        this.taskList = document.getElementById("task");
+        this.taskListChildren = this.taskList.children;
+        this.addButton = document.getElementById("add_task_button");
+        this.errorMessage = document.getElementById("error");
+    },
 
-    complete()
+    createTask: function() 
     {
-        this.completed = true;
-        this.icon = "fas fa-check-circle";
-    }    
-}
+        //create blank variables
+        let taskListItem, taskCheckbox, taskValue, taskButton, taskTrash;
+
+        //create the list item
+        taskListItem = document.createElement("li");
+        taskItem.setAttribute("class", "task")
+
+        //Create the checkbox
+        taskCheckbox = document.createElement("input");
+        taskCheckbox.setAttribute("type", "checkbox")
+
+        //create the task value
+        taskValue = document.createTextNode(this.taskInput.value);
+
+        // create the delete button
+        taskButton = document.createElement("button");
+
+        //create the X icon
+        taskTrash = document.createElement("i");
+        taskTrash.setAttribute("class", "fas fa-times-circle");
+
+        //connect x Icon with delete button
+        taskButton.appendChild(taskTrash);
+
+        //connect elements to the list
+        taskListItem.appendChild(taskCheckbox);
+        taskListItem.appendChild(taskValue);
+        taskListItem.appendChild(taskButton);
+
+        //add actual task to the list
+        this.taskList.appendChild(taskListItem);
+    },
+
+    error: function()
+    {
+        this.errorMessage.style.display = "block";
+    },
+
+    addTask: function()
+    {
+        let taskValue = this.taskInput.value;
+        this.errorMessage.style.display = "none"
+
+        if(taskValue === "")
+        {
+            this.error();
+        }
+        else
+        {
+            this.buildTask();
+            this.taskInput.value = "";
+        }
+    }
+};
+//         this.toDo = item;
+//         this.id = id;
+//         this.trash = false;
+//         this.completed = false;
+//         this.icon = "far fa-arrow-alt-circle-right";
+//     }
+
+//     complete()
+//     {
+//         this.completed = true;
+//         this.icon = "fas fa-check-circle";
+//     }    
+// }
 
 
 //To do adds on enter
@@ -69,7 +137,7 @@ function addItem(item)
     
 
     const toDoItem = `<li class="item" >
-                    <i class="${item.icon}" id="${item.id}"></i>
+                    <i class="${item.icon}" id="icon"></i>
                     <p class="text"> ${item.toDo}</p>
                     <i class="fas fa-times-circle"</i>
                     </li>`
