d98b36d8e71e8e0101abc70766d151f1fd0c1cad
stchambers89 <56280693+stchambers89@users.noreply.github.com>
2020-10-30 18:00:42 -0700

    small tweaks

--- week5/tasker.js
+++ week5/tasker.js
@@ -66,7 +66,7 @@ function buildTask(task)
 
 document.addEventListener("keyup", function(event)
 {
-    if(event.code == "Enter")
+    if(event.key == "Enter")
     {
         addTask();
     }
@@ -86,6 +86,7 @@ function addTask()
         id++;
     }
     input.value = "";
+    document.getElementById("input").focus();
 }
 
 /**********************************
