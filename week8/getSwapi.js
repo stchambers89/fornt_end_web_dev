5c14a2ab6dedac312f14df1bb48f6fd946b90910
stchambers89 <56280693+stchambers89@users.noreply.github.com>
2020-11-04 18:56:37 -0800

    Consuming api

---
+++ week8/getSwapi.js
@@ -0,0 +1,27 @@
+let list = [];
+
+document.getElementById('getApi').addEventListener('click', getApi);
+
+function getApi()
+{
+    fetch('https://swapi.dev/api/planets/')
+    .then( (response) => response.json())
+    .then((data) => {
+        let output = ''; // just put into html
+        console.log(data.results)
+        data.results.forEach(function(planet){
+            output += ` <div class="planet_card">
+                        <ul> 
+                            <li class="planet_name"> ${planet.name} </li>
+                            <li> Climate: ${planet.climate} </li>
+                            <li> Terrain: ${planet.terrain} </li>
+                            <li> Population: ${planet.population} </li>
+                            <li> Planetary Rotation: ${planet.rotation_period} hours </li>
+                            <li> Full solar rotaion: ${planet.orbital_period} days</li>  
+                        </ul>
+                        </div>`;
+        });
+
+        document.getElementById('output').innerHTML = output;
+     });
+}
