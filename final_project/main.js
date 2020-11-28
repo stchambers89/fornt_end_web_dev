b1b606f95dfd6bec6045c45e566647249f73b840
stchambers89 <56280693+stchambers89@users.noreply.github.com>
2020-11-28 15:13:37 -0800

    connected DND api created json for class intro

--- final_project/main.js
+++ final_project/main.js
@@ -1,18 +1,45 @@
-const coreURL = 'https://www.dnd5eapi.co/api/races';
+const coreURL = 'https://www.dnd5eapi.co/api/';
+const raceList = ['dragonborn', 'dwarf', 'elf', 'gnome', 'half-elf', 'half-orc', 'halfling', 'human', 'tiefling'];
+const classesList = ['barbarian', 'bard', 'cleric', 'druid', 'fighter', 'monk', 'ranger',
+'rouge', 'sorcerer', 'warlock', 'wizard'];
 
 
 
 
 
-
-
-
-async function readApi()
+async function readClasses(classes)
 {
     console.log(`calling read api......`);
-    const incomingResponse = await fetch(coreURL);
+    const incomingResponse = await fetch('classes.json');
     const data = await incomingResponse.json();
     console.log(data);
+
+    let output = `
+                    ${data.class[0].intro}
+                    
+                `;
+
+    document.getElementById(classes+"_info").innerHTML = output;
+    
+    
+}
+
+async function readRaceApi(race)
+{
+    console.log(`calling read api......`);
+    const incomingResponse = await fetch(`${coreURL}races/${race}/`);
+    const data = await incomingResponse.json();
+    console.log(data);
+
+    let output = `
+                    <p> Lifespan: ${data.age} </p>
+                    <p> Alignment: ${data.alignment} </p>
+                    <p> Size: ${data.size_description} </p>
+                    <p> Languages: ${data.language_desc} </p>
+                    
+                `;
+
+    document.getElementById(race+"_info").innerHTML = output;
     
     
 }
@@ -21,13 +48,28 @@ const coll = document.getElementsByClassName("collapsible");
 
 for (let card of coll){
   card.addEventListener("click", function() {
-    this.classList.toggle("active", "test");
-    var content = this.nextElementSibling;
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
     if (content.style.maxHeight){
       content.style.maxHeight = null;
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
     } 
+    let contentName = content.id;
+    console.log(contentName);
+    if(raceList.some(a => a === contentName))
+    {
+      readRaceApi(contentName);
+    }
+    else{
+      console.log(`you clicked the ${contentName} class`)
+      readClasses(contentName)
+    }
+    
   });
 }
+
+let barbarian = document.getElementById('barbarian_info');
+
+
 
