d9a5529412d1e1a0dc12215726152cd67bd1587c
stchambers89 <56280693+stchambers89@users.noreply.github.com>
2020-12-01 19:55:27 -0800

    Updating class selector

--- final_project/main.js
+++ final_project/main.js
@@ -1,41 +1,42 @@
 const coreURL = 'https://www.dnd5eapi.co/api/';
 const raceList = ['dragonborn', 'dwarf', 'elf', 'gnome', 'half-elf', 'half-orc', 'halfling', 'human', 'tiefling'];
+const classList = ['barbarian', 'bard', 'cleric', 'druid', 'fighter', 'monk', 'ranger', 'rouge', 'sorcerer', 'warlock', 'wizard'];
+const classLength = classList.length;
 
 
 
 
+async function readClasses(playerClass){
+  const incomingResponse = await fetch('playerClass.json');
+  const data = await incomingResponse.json();
+  let index;
+  for(let i = 0; i < classLength; i++){
+    if(playerClass === classList[i]){
+      index = i;
+    }
+  }
 
+  let output = `
+                  ${data.class[index].intro}
+                  ${data.class[index].history}
+                  ${data.class[index].personality}
+                  ${data.class[index].creation}
+                  
+              `;
 
-async function readClasses(playerClass)
-{
-    console.log(`calling class Json......`);
-    const incomingResponse = await fetch('playerClass.json');
-    const data = await incomingResponse.json();
-    console.log(data);
-    // need to see if passes in the player class, then loop through with conditional playerClass === class[i].class
-    //may want to edit the json for readability. 
-    let output = `
-                    ${data.class[0].intro}
-                    ${data.class[0].history}
-                    ${data.class[0].personality}
-                    ${data.class[0].creation}
-                    
-                `;
-
-    document.getElementById(`${playerClass}_info`).innerHTML = output; 
+  document.getElementById(`${playerClass}_info`).innerHTML = output; 
 }
 
 async function readRaceApi(race)
 {
-  console.log(`calling read api......`);
   const incomingResponse = await fetch(`${coreURL}races/${race}/`);
   const data = await incomingResponse.json();
   console.log(data);
 
-  let output = `  <p> <h3>Lifespan</h3><hr class="line"> ${data.age} </p>
-                  <p> <h3>Alignment</h3><hr class="line"> ${data.alignment} </p>
-                  <p> <h3>Size</h3><hr class="line"> ${data.size_description} </p>
-                  <p> <h3>Languages</h3><hr class="line"> ${data.language_desc} </p>`;
+  let output = `  <p> <span class='headText'>Lifespan</span><hr class="line"> ${data.age} </p>
+                  <p> <span class='headText'>Alignment</span><hr class="line"> ${data.alignment} </p>
+                  <p> <span class='headText'>Size</span><hr class="line"> ${data.size_description} </p>
+                  <p> <span class='headText'>Languages</span><hr class="line"> ${data.language_desc} </p>`;
 
   document.getElementById(`${race}_info`).innerHTML = output;
     
@@ -54,20 +55,17 @@ for (let card of coll){
     } 
     else 
     {
-      content.style.maxHeight =  "350px";
-      content.style.overflow = "auto";
+      content.style.maxHeight =  "400px";
+      content.style.overflow.y = "scroll";
     } 
     let contentName = content.id;
-    console.log(contentName);
     if(raceList.some(a => a === contentName))
     {
-      console.log(`Race selceted and fired`)
       readRaceApi(contentName);
     }
     else
     {
-      console.log(`class selceted and fired`)
-      readClasses(contentName)
+      readClasses(contentName);
     }
     
   });
